78c9dac4f6c3a0ae944680729b1cba93d9a361f6
Icar0S <icarooliveira_23@hotmail.com>
2024-12-06 19:08:52 -0300

    remove comments

--- scripts-miner-sonatype/script_clean.php
+++ scripts-miner-sonatype/script_clean.php
@@ -1,33 +1,32 @@
 <?php
 
-$linhas = explode("\n", `cat artefatos_categorizados_v7.log`); // Lê o arquivo e o transforma em um array de linhas
+$linhas = explode("\n", `cat artefatos_categorizados_v7.log`);
 
 
 $inicio = true;
 
 foreach ($linhas as $linha) {
     $linha = trim($linha);
-    $partes = explode("#", $linha); // Divide a linha pelo caractere #
-    
+    $partes = explode("#", $linha); 
     
     foreach ($partes as $parte) {
 	
 	    if($inicio) {
-		// Armazenando o groupID    
+		
 		$groupId = explode(":", $parte)[0];
 		echo $parte;
 		$inicio = false;
 		continue;
 	}
 
-	// Eliminando a hashtag GroupID
+	
         if (trim($parte) != $groupId) { 
             echo "#".$parte;
         }
     }
     $inicio = true;
     $groupId = "";
-    echo "\n"; // Adiciona uma linha em branco entre as linhas processadas
+    echo "\n"; 
 }
 
 ?>
--- scripts-miner-sonatype/script_search.py
+++ scripts-miner-sonatype/script_search.py
@@ -7,58 +7,64 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import concurrent.futures
-import time
 
-def fetch_categories(artefato):
-    url = f"https://central.sonatype.com/search?q={artefato.replace(':', '%3A')}"
-    
-    # Iniciar um novo driver dentro da função
+
+def fetch_categories(artifact):
+    url = f"https://central.sonatype.com/search?q={artifact.replace(':', '%3A')}"
+
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service('/usr/local/bin/chromedriver')
+    service = Service("/usr/local/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    
+
     try:
         driver.get(url)
         categories_elements = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, 'nx-tag__text'))
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "nx-tag__text"))
         )
-        return [category.text for category in categories_elements] if categories_elements else None
+        return (
+            [category.text for category in categories_elements]
+            if categories_elements
+            else None
+        )
     except Exception as e:
-        # print(f"Erro ao buscar categorias: {e}")
         return None
     finally:
         driver.quit()
 
+
 def main(file_path):
-    with open(file_path, 'r') as file:
-        artefatos = file.readlines()
+    with open(file_path, "r") as file:
+        artifacts = file.readlines()
 
-    # Utilizando ThreadPoolExecutor para processar em paralelo
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_artefato = {executor.submit(fetch_categories, artefato.strip()): artefato.strip() for artefato in artefatos if artefato.strip()}
+        future_to_artifact = {
+            executor.submit(fetch_categories, artifact.strip()): artifact.strip()
+            for artifact in artifacts
+            if artifact.strip()
+        }
 
-        for future in concurrent.futures.as_completed(future_to_artefato):
-            artefato = future_to_artefato[future]
+        for future in concurrent.futures.as_completed(future_to_artifact):
+            artifact = future_to_artifact[future]
             try:
                 categories = future.result()
                 if categories is None:
-                    print(f"{artefato};#err_tag_not_found")
+                    print(f"{artifact};#err_tag_not_found")
                 else:
-                    formatted_categories = ';'.join(categories)
-                    print(f"{artefato}#{formatted_categories}")
+                    formatted_categories = ";".join(categories)
+                    print(f"{artifact}#{formatted_categories}")
                 sys.stdout.flush()
             except Exception as e:
-                print(f"Erro ao processar {artefato}: {e}")
+                print(f"Erro ao processar {artifact}: {e}")
                 sys.stdout.flush()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Processa artefatos do arquivo CSV.')
-    parser.add_argument('file_path', type=str, help='Caminho do arquivo CSV com os artefatos.')
-    args = parser.parse_args()
-    
-    main(args.file_path)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="process artifact in file CSV.")
+    parser.add_argument("file_path", type=str, help="path of file CSV with artifacts.")
+    args = parser.parse_args()
+
+    main(args.file_path)
